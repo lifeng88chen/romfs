563073d6d22897bb05bee802da48c6082c54065f
lifeng chen <lifeng88chen@gmail.com>
2019-02-28 16:41:45 +0800

    romfs:add super block operations
    This commit adds romfs super block operations.

---
+++ super.c
@@ -0,0 +1,5 @@
+module_init(romfs_module_init);
+module_exit(romfs_module_exit);
+MODULE_DESCRIPTION("EXTERN Read only filesystem");
+MODULE_AUTHOR("Chen Lifeng lifeng88chen@gmail.com");
+MODULE_LICENSE("GPL");
