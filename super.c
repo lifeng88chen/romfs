34919045d5e9bab0a3b72073181c6e45b8d7f94a
lifeng chen <lifeng88chen@gmail.com>
2019-03-18 19:37:20 +0800

    add fill super
    Signed-off-by: lifeng chen <lifeng88chen@gmail.com>

--- super.c
+++ super.c
@@ -1,24 +1,42 @@
+/*
+   1.init sb->sb_info;
+   2.init sb->uuid;
+*/
+
+static int superblock_read(struct super_block *sb)
+{
+	struct buffer_head *bh;
+	bh = sb_read(sb,0);
+	struct erofs_super_block *layout;
+	layout = (struct romfs_super_block *)(bh->data+EROFS_SUPER_OFFSET);
+}
 
 static int romfs_read_super(struct super_block *sb,
-	const char *dev_name, void *data, int silent)
+	const char *dev_name, void *options, int silent)
 {
-	
+        
+	/*1.read super block on the disk*/
+	bh = sb_read(sb,0);
 }
 
 
 static int romfs_fill_super(struct super_block *sb,
-	void *_priv, int silent)
+	void *priv, int silent)
 {
-	return romfs_read_super(sb, NULL,
-		NULL, silent);
+	return romfs_read_super(sb, priv->dev_name,
+		priv->options, silent);
 }
-
+/*int (*fill_super)(struct super_block *, void *, int)*/
 static struct dentry *romfs_mount(
 	struct file_system_type *fs_type, int flags,
 	const char *dev_name, void *data)
 {
+        struct erofs_mount_private priv = {
+		.dev_name = dev_name,
+		.options = data
+		};
 	return mount_bdev(fs_type, flags, dev_name,
-		NULL, romfs_fill_super);
+		priv, romfs_fill_super);
 }
 
 static void romfs_kill_sb(struct super_block *sb)
